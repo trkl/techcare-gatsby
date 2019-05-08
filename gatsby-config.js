e619c8a1741e47e85a4c3be158da24464857c9c6
Torkil <torkildjurhuus@gmail.com>
2019-05-08 15:10:08 +0100

    added mailchimp signup to website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Photon",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Photon by HTML5 UP"
+    title: 'Gatsby Starter - Photon',
+    author: 'Hunter Chang',
+    description: 'A Gatsby.js Starter based on Photon by HTML5 UP'
   },
+
   plugins: [
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+        endpoint:
+          'https://techcare.us20.list-manage.com/subscribe/post?u=d4bfd58ff7fa6936df11c0095&amp;id=81e17a31a6' // add your MC list endpoint here; see instructions below
+      }
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
@@ -15,10 +23,10 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
-      },
+        icon: 'src/assets/images/website-icon.png' // This path is relative to the root of the site.
+      }
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
-  ],
-}
+  ]
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React from 'react';
+import addToMailchimp from 'gatsby-plugin-mailchimp';
 
 class Header extends React.Component {
+  state = {
+    email: ''
+  };
+
+  handleInputChange = event => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  _handleSubmit = async event => {
+    console.log(this.state.email);
+    event.preventDefault();
+    const result = await addToMailchimp(this.state.email);
+    // I recommend setting `result` to React state
+    // but you can do whatever you want
+  };
+
   render() {
     return (
       <section id="header">
@@ -14,18 +37,29 @@ class Header extends React.Component {
             <br />
             <br />
           </h1>
-          <form>
-            <input
-              id="id01"
-              type="text"
-              name="email"
-              placeholder="example@mail.com"
-            />
+          <form onSubmit={this._handleSubmit}>
+            <div>
+              <input
+                id="id01"
+                type="text"
+                name="email"
+                placeholder="example@mail.com "
+                value={this.state.email}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div />
+
+            <div>
+              <button id="newsletterSubmit" type="submit">
+                Submit
+              </button>
+            </div>
           </form>
         </div>
       </section>
-    )
+    );
   }
 }
 
-export default Header
+export default Header;
